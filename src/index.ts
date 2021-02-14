32b4063689e7b4c29973547bf8670ef84918df2a
Harold Iedema <harold@iedema.me>
2021-02-14 16:24:38 +0100

    Updated README.md

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-/* Byteshift Elements                                                              _         _             __   _ _____
- *    A self-encapsulating WebComponent framework                                 | |__ _  _| |_ ___  ___ / /  (_) _/ /_
+/* Byteshift Harmony                                                               _         _             __   _ _____
+ *    A component-based HTTP server micro-framework                               | |__ _  _| |_ ___  ___ / /  (_) _/ /_
  *                                                                                | '_ \ || |  _/ -_|(_-</ _ \/ / _/ __/
  * (C)2020, Harold Iedema <harold@iedema.me>                                      |_.__/\_, |\__\___/___/_//_/_/_/ \__/
- * See LICENSE for licensing information                                                |__/                   H T T P
+ * See LICENSE for licensing information                                                |__/             H A R M O N Y
  */
 'use strict';
 
